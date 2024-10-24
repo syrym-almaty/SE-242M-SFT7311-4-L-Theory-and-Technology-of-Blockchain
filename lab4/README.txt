9d34f175c651edab5ee43dbd61287aef0a4a54e8
Ilkham <116306492+Munoto@users.noreply.github.com>
2024-10-24 21:35:42 +0500

    Add files via upload

---
+++ lab4/README.txt
@@ -0,0 +1,5 @@
+LAB 4
+
+Student: Massimov ilkham
+
+Used REMIX IDE
---
+++ lab4/contracts/test.sol
@@ -0,0 +1,8 @@
+pragma solidity >=0.6.0 <0.9.0;
+
+contract HelloWorld {
+    function helloWorld() external pure returns (string memory) {
+        return "Hello, World!";
+    }
+}
+
