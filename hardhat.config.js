c13c886c84b0482e0dcc4684ae2715ade52ec1d9
TheElderLord <61541224+TheElderLord@users.noreply.github.com>
2024-10-27 23:38:41 +0500

    Commiting the result

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity: "0.8.0",
 };
+
